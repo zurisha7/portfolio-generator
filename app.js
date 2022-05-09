0f4634277bb4faf4b139a97cf4c652dbccd57c85
Zurisha <andreamwest7@gmail.com>
2022-05-09 17:06:09 -0500

    learn HTML in node

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
-const printProfileData = (profileDataArr) => {
-    for(let i= 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-}
-console.log('===============');
+// const printProfileData = (profileDataArr) => {
+//     for(let i= 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+// }
+// console.log('===============');
 
-// is the same as this
+// // is the same as this
 
-    profileDataArr.forEach((profileItem => console.log(profileItem));
+//     profileDataArr.forEach((profileItem => console.log(profileItem));
     
-};    
+// };  
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Portfolio Demo</title>
+    </head>
+  
+    <body>
+      <h1>${name}</h1>
+      <h2><a href="https://github.com/${github}">Github</a></h2>
+    </body>
+    </html>
+    `;
+  };
+console.log(name, github);
+console.log(generatePage(name, github));
 
+9.2.5 node API
